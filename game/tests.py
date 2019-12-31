983233abe9fb9531d3c9bbac6cedcde9023716be
Jean-Philippe Boudreault <leconcombre@gmail.com>
2019-12-30 20:22:32 -0500

    Tests and isolated the boardgame status

--- game/tests.py
+++ game/tests.py
@@ -1,40 +1,55 @@
 from django.test import SimpleTestCase
 
-from game.models import Game
+from game.transients import Board
+
+
+def sample_input():
+    return [
+        'x', 'x', ' ',
+        'o', ' ', ' ',
+        'o', ' ', 'x',
+    ]
 
 
 class GameTest(SimpleTestCase):
-    def test_get_lines(self):
-        inputs = [
-            'x', 'x', ' ',
-            'o', ' ', ' ',
-            'o', ' ', 'x',
-        ]
-        board = Game(inputs)
-        lines = board.get_lines()
-        self.assertEqual(lines[0], ['x', 'x', ' '])
-        self.assertEqual(lines[1], ['o', ' ', ' '])
-        self.assertEqual(lines[2], ['o', ' ', 'x'])
+
+    def test_get_rows(self):
+        lines = Board(sample_input()).get_rows()
+        self.assertEqual(lines[0].get_data(), ['x', 'x', ' '])
+        self.assertEqual(lines[0].get_positions(), [0, 1, 2])
+        self.assertEqual(lines[1].get_data(), ['o', ' ', ' '])
+        self.assertEqual(lines[1].get_positions(), [3, 4, 5])
+        self.assertEqual(lines[2].get_data(), ['o', ' ', 'x'])
+        self.assertEqual(lines[2].get_positions(), [6, 7, 8])
 
     def test_get_columns(self):
-        inputs = [
-            'x', 'x', ' ',
-            'o', ' ', ' ',
-            'o', ' ', 'x',
-        ]
-        board = Game(inputs)
-        columns = board.get_columns()
-        self.assertEqual(columns[0], ['x', 'o', 'o'])
-        self.assertEqual(columns[1], ['x', ' ', ' '])
-        self.assertEqual(columns[2], [' ', ' ', 'x'])
+        columns = Board(sample_input()).get_columns()
+        self.assertEqual(columns[0].get_data(), ['x', 'o', 'o'])
+        self.assertEqual(columns[0].get_positions(), [0, 3, 6])
+        self.assertEqual(columns[1].get_data(), ['x', ' ', ' '])
+        self.assertEqual(columns[1].get_positions(), [1, 4, 7])
+        self.assertEqual(columns[2].get_data(), [' ', ' ', 'x'])
+        self.assertEqual(columns[2].get_positions(), [2, 5, 8])
 
     def test_get_diagonals(self):
-        inputs = [
-            'x', 'x', ' ',
-            'o', ' ', ' ',
-            'o', ' ', 'x',
-        ]
-        board = Game(inputs)
-        diagonals = board.get_diagonals()
-        self.assertEqual(diagonals[0], ['x', ' ', 'x'])
-        self.assertEqual(diagonals[1], [' ', ' ', 'o'])
+        diagonals = Board(sample_input()).get_diagonals()
+        self.assertEqual(diagonals[0].get_data(), ['x', ' ', 'x'])
+        self.assertEqual(diagonals[0].get_positions(), [0, 4, 8])
+        self.assertEqual(diagonals[1].get_data(), [' ', ' ', 'o'])
+        self.assertEqual(diagonals[1].get_positions(), [2, 4, 6])
+
+    def test_get_lines(self):
+        tuples = Board(sample_input()).get_lines()
+        self.assertEqual(len(tuples), 8)
+
+    def test_is_game_over(self):
+        self.assertFalse(Board(sample_input()).is_game_over())
+
+        all_x = ['x'] * 9
+        self.assertTrue(Board(all_x).is_game_over())
+
+    def test_get_winner(self):
+        self.assertEquals(Board(sample_input()).get_winner(), None)
+
+        all_x = ['x'] * 9
+        self.assertEquals(Board(all_x).get_winner(), 'x')
---
+++ game/transients.py
@@ -0,0 +1,67 @@
+import copy
+
+
+class Board:
+    def __init__(self, data):
+        self.data = copy.deepcopy(data)
+
+    def get_rows(self):
+        return [
+            Line(self, [0, 1, 2]),
+            Line(self, [3, 4, 5]),
+            Line(self, [6, 7, 8]),
+        ]
+
+    def get_columns(self):
+        return [
+            Line(self, [0, 3, 6]),
+            Line(self, [1, 4, 7]),
+            Line(self, [2, 5, 8]),
+        ]
+
+    def get_diagonals(self):
+        return [
+            Line(self, [0, 4, 8]),
+            Line(self, [2, 4, 6]),
+        ]
+
+    def get_lines(self):
+        return [] + self.get_rows() + self.get_columns() + self.get_diagonals()
+
+    def get_winner(self):
+        for line in self.get_lines():
+            if line.get_winner() is not None:
+                return line.get_winner()
+        return None
+
+    def is_game_over(self):
+        if self.get_winner() is not None:
+            return True
+        if all(r.is_filled() for r in self.get_rows()):
+            return True
+        return False
+
+
+class Line:
+    def __init__(self, game, positions):
+        self.positions = copy.deepcopy(positions)
+        self.data = []
+        for position in positions:
+            self.data.append(game.data[position])
+
+    def get_data(self):
+        return self.data
+
+    def get_positions(self):
+        return self.positions
+
+    def get_winner(self):
+        if all(d == 'x' for d in self.data):
+            return 'x'
+        elif all(d == 'o' for d in self.data):
+            return 'o'
+        else:
+            return None
+
+    def is_filled(self):
+        return all(d != ' ' for d in self.data)
