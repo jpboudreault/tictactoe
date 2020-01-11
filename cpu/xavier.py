3e49a0b4cf37adf6ccae8f5d2ac214eb443a8116
Jean-Philippe Boudreault <leconcombre@gmail.com>
2020-01-11 01:10:36 -0500

    ignore the db sql file

--- cpu/xavier.py
+++ cpu/xavier.py
@@ -21,3 +21,23 @@ class Xavier(Cpu):
             return 5;
 
         return 5
+    #
+    # winning_position=self.find_winning_position
+    # if winning_position:
+    #     return winning_position
+
+    def find_winning_position(self, board: Board, side_to_match):
+        for line in board.get_lines():
+            if not line.is_filled():
+                matches = self.find_matching_positions(board.data, line.get_positions(), side_to_match)
+                if len(matches) == 2:
+                    return [item for item in line.get_positions() if item not in matches][0]
+        return None
+
+    def find_matching_positions(self, data, positions, side_to_match):
+        matches = []
+        for p in positions:
+            if data[p] == side_to_match:
+                matches.append(p)
+
+        return matches
