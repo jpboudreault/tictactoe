578df9f209f790120d6bc4b689bb3b2032e8b4e3
Jean-Philippe Boudreault <leconcombre@gmail.com>
2020-01-11 00:46:06 -0500

    moved services around cpu to another app

--- game/services.py
+++ game/services.py
@@ -1,17 +1,27 @@
-from cpu.center import Center
-from cpu.first import First
-from cpu.random import Random
-from cpu.xavier import Xavier
+from ast import literal_eval
+
+from .models import *
+from cpu.services import *
 
 
-def get_ai(code):
-    if code == 'first':
-        return First()
-    elif code == 'rand':
-        return Random()
-    elif code == 'xav':
-        return Xavier()
-    elif code == 'center':
-        return Center()
-    else:
-        raise ModuleNotFoundError(code)
+def game_2_board(game: Game):
+    # go from a list of moves to a data array ['x', ' ', 'o', ...]
+    moves = literal_eval(game.moves)
+    data = [' '] * 9
+    for num, move in enumerate(moves, start=0):
+        data[move] = 'x' if num % 2 == 0 else 'o'
+
+    return Board(data)
+
+
+def play_cpu_if_needed(game: Game):
+    cpu = get_cpu(game.cpu_code)
+    moves = literal_eval(game.moves)
+    board = game_2_board(game)
+
+    if game.cpu_first_player and len(moves) % 2 == 0:
+        moves.append(cpu.play(board, 'x', 'o'))
+    elif not game.cpu_first_player and len(moves) % 2 == 1:
+        moves.append(cpu.play(board, 'o', 'x'))
+
+    game.moves = "%s" % moves
