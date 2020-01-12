0098a1b13599ec4a55a57fbce2637e5bfe2f802a
Jean-Philippe Boudreault <leconcombre@gmail.com>
2020-01-12 08:59:15 -0500

    added cpu 2 in the model

--- game/migrations/0001_initial.py
+++ game/migrations/0001_initial.py
@@ -13,9 +13,10 @@ class Migration(migrations.Migration):
             name='Game',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('moves', models.CharField(max_length=9)),
+                ('moves', models.CharField(max_length=40)),
                 ('cpu_code', models.CharField(max_length=100)),
                 ('cpu_first_player', models.BooleanField()),
+                ('cpu2_code', models.CharField(max_length=100)),
             ],
         ),
     ]
--- game/models.py
+++ game/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Game(models.Model):
-    moves = models.CharField(max_length=9)
+    moves = models.CharField(max_length=40)
     cpu_code = models.CharField(max_length=100)
     cpu_first_player = models.BooleanField()
+    cpu2_code = models.CharField(max_length=100)
