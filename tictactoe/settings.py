f7fa3d090454e274fb2619bbf9c706b678e05899
Jean-Philippe Boudreault <jean-philippe.boudreault@appdirect.com>
2020-01-17 20:13:45 -0500

    Removing unneeded depencies

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -31,14 +31,12 @@ DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.sqlite3',
     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  #  'NAME': ':memory:',
   }
 }
 
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4',
     'game.apps.GameConfig',
     'cpu.apps.CpuConfig',
     'django.contrib.auth',
